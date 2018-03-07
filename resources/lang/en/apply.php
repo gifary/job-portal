3ef80023e0122c79629d705f767c1b144d9bf936
Muhammad Gifaary <muhammadgifary@gmail.com>
2018-03-07 15:44:02 +0700

    fix

--- resources/lang/en/apply.php
+++ resources/lang/en/apply.php
@@ -23,4 +23,20 @@ return [
     'lainnya' => "Other",
     'nama_sekolah' => "School Name",
     'nilai_sekolah' => "Score/GPA",
+    'academy_text' => '<p  style="font-weight: bold; text-align: justify;">
+                <h3><b>Do you want to start your career as Hotelier? </b></h3>
+            </p>
+            <p  style="font-weight: bold; text-align: justify;">
+                <b>Let’s join us at SAS Hotel Academy! Get yourself passionate to learning, commitment, and high integrity. You do not to spend any money to afford. JUST FREE!!</b>
+            </p>
+            <p style="text-align: justify;">
+                SAS Hotel Academy provides an opportunity for you to adjust your educational background with professional hospitality career. Whoever you are and which level of education are you, you are welcome to join the academy.
+            </p>
+            <p style="text-align: justify;">
+                Learning from the best professionals hotelier will help you to be ready to work in the world of tourism career, especially hospitality. In-class training and field practice at our prestigious stars hotels would opens up your chances of getting ready to work.
+            </p>
+            <p style="text-align: justify;">
+                Many of our graduates are able to work in hospitality with adequate skills that have been taught throughout the learning process. For 3 months you will be On The Job Training (OJT) with the benefit of one meal a day. After you run the OJT phase, then you go into the Internship process for 9 months with Rp 1,500,000 (one million and five hundred thousand rupiah) allowance.
+            </p>',
+    'apply_academy'=> 'JOIN'
 ];
--- resources/lang/id/apply.php
+++ resources/lang/id/apply.php
@@ -23,4 +23,20 @@ return [
     'lainnya' => "Lainnya",
     'nama_sekolah' => "Nama Sekolah",
     'nilai_sekolah' => "IPK/Nilai SKHUN",
+    'academy_text' => '<p  style="font-weight: bold; text-align: justify;">
+                <h3><b>Anda ingin dapat bekerja di dunia perhotelan? </b></h3>
+            </p>
+            <p  style="font-weight: bold; text-align: justify;">
+                <b>Mari bergabung bersama kami di SAS Hotel Academy! Hanya bermodalkan kemauan belajar, komitmen, dan integritas. Teman-teman tidak perlu mengeluarkan sepeserpun. GRATIS!! </b>
+            </p>
+            <p style="text-align: justify;">
+                SAS Hotel Academy memberikan kesempatan bagi kamu untuk menjembatani pendidikan kamu dengan dunia profesional atau karir. Siapapun kamu dan level pendidikan kamu dapat kami terima untuk bergabung bersama akademi. 
+            </p>
+            <p style="text-align: justify;">
+                Belajar dari para profesional terbaik akan membantu kamu untuk siap bekerja di dunia pariwisata khususnya perhotelan. Pelatihan dalam kelas dan praktek secara langsung di hotel-hotel berbintang kami yang prestisius membuka peluang kamu agar siap bekerja. 
+            </p>
+            <p style="text-align: justify;">
+                Lulusan kami sudah banyak yang mampu bekerja di perhotelan dengan keterampilan memadai yang telah diajarkan selama proses pembelajaran berlangsung. Selama 3 bulan kamu akan praktek lapangan atau On The Job Training (OJT) dengan manfaat makan di kantin satu kali sehari. Setelah kamu lolos fase OJT, selanjutnya kamu masuk proses Magang atau Internship selama 9 bulan dengan diberikan uang saku sebesar Rp 1.500.000 (satu juta lima ratus ribu rupiah).
+            </p>',
+    'apply_academy'=> 'DAFTAR'
 ];
